3d45ce66618c5ec228daffc46e0eaf7c9713d34e
DUVAL <theophane.duval@capgemini.com>
2020-03-30 14:16:45 +0200

    Adding serachBar for areas

--- src/components/home.js
+++ src/components/home.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import ItemAreaList from './itemAreaList'
 import {Link} from "react-router-dom"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+
 
 class Home extends React.Component {
 
     constructor(props){
         super(props)
+        this.state = {
+            searchValue: ''
+        }
+        this.handleChangeSearch = this.handleChangeSearch.bind(this)
+    }
+
+    handleChangeSearch(event){
+        this.setState({
+            searchValue: event.target.value
+        })
     }
 
     render(){
@@ -14,12 +27,20 @@ class Home extends React.Component {
 
         if (areas.length > 0){
             for(let i = 0; i < areas.length; i++) {
+                if (areas[i].name.toUpperCase().indexOf(this.state.searchValue.toUpperCase()) > -1){
+                    myList.push(
+                        <ItemAreaList id={i} 
+                            editArea={this.props.editArea}
+                            deleteArea={this.props.deleteArea}
+                            area={areas[i]}
+                        />
+                    )
+                }
+            } if (myList.length === 0){
                 myList.push(
-                    <ItemAreaList id={i} 
-                        editArea={this.props.editArea}
-                        deleteArea={this.props.deleteArea}
-                        area={areas[i]}
-                    />
+                    <tr>
+                        <td colSpan={5}>Aucune zone correspondante.</td>
+                    </tr>
                 )
             }
         } else {
@@ -32,6 +53,9 @@ class Home extends React.Component {
 
         return (
             <div className="home">
+                <div className="searchBar">
+                    <input type='text' onChange={this.handleChangeSearch} placeholder="Chercher une zone"/><FontAwesomeIcon icon={faSearch}/>
+                </div>
                 <table cellspacing="0" cellpadding="0">
                     <thead>
                         <tr>
